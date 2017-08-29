15bd2d108344b9c0a5301a2520b5669449a91101
danielyaa5 <danielyaa5@gmail.com>
2017-08-28 23:21:02 -0700

    demo page updates

--- docs/demo.js
+++ docs/demo.js
@@ -4,16 +4,69 @@ import CreativeBtn from '../dist/CreativeBtn';
 import './demo.css';
 
 class Demo extends React.PureComponent {
+  componentWillUnmount() {
+    this.ref.parentNode.style.background = this.originalBg;
+  }
+
+  handleRef = (ref) => {
+    if (!this.ref) {
+      this.ref = ref;
+      this.originalBg = ref.parentNode.style.background;
+      ref.parentNode.style.background = 'rgb(14,131,205)';
+    }
+  };
   render() {
     return (
-      <div className="demo-root" >
+      <div className="demo-root" ref={this.handleRef}>
         <div className="demo-btns-container" >
-          <CreativeBtn txt="Hello World" effect="1a" />
-          <CreativeBtn txt="Hello World" effect="1b" />
-          <CreativeBtn txt="Hello World" effect="1c" />
-          <CreativeBtn txt="Hello World" effect="1d" />
-          <CreativeBtn txt="Hello World" effect="1e" />
-          <CreativeBtn txt="Hello World" effect="1f" />
+          <CreativeBtn
+            txt="Hello World!"
+            effect="1a"
+            bg="rgb(14,131,205)"
+            hoverColor="rgb(14,131,205)"
+            hoverBg="white"
+            color="white"
+          />
+          <CreativeBtn
+            txt="Hello World!"
+            effect="1b"
+            bg="rgb(14,131,205)"
+            hoverColor="rgb(14,131,205)"
+            hoverBg="white"
+            color="white"
+          />
+          <CreativeBtn
+            txt="Hello World!"
+            effect="1c"
+            bg="rgb(14,131,205)"
+            hoverColor="rgb(14,131,205)"
+            hoverBg="white"
+            color="white"
+          />
+          <CreativeBtn
+            txt="Hello World!"
+            effect="1d"
+            bg="rgb(14,131,205)"
+            hoverColor="rgb(14,131,205)"
+            hoverBg="white"
+            color="white"
+          />
+          <CreativeBtn
+            txt="Hello World!"
+            effect="1e"
+            bg="rgb(14,131,205)"
+            hoverColor="rgb(14,131,205)"
+            hoverBg="white"
+            color="white"
+          />
+          <CreativeBtn
+            txt="Hello World!"
+            effect="1f"
+            bg="rgb(14,131,205)"
+            hoverColor="rgb(14,131,205)"
+            hoverBg="white"
+            color="white"
+          />
         </div >
       </div >
     );
